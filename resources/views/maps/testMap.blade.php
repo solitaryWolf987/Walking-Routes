3db92293b492b74422c35ed7bdb4765635557661
SolitaireWolf <45793465+solitaryWolf987@users.noreply.github.com>
2022-02-26 14:31:58 +0000

    search bar that outputs coords in console
    need to get them out of console and like them to the map

--- resources/views/maps/testMap.blade.php
+++ resources/views/maps/testMap.blade.php
@@ -1,3 +1,26 @@
 
 
-<iframe width="1000" height="750" src="https://api.maptiler.com/maps/outdoor/?key=kVbYzZdvpCATj1RhoWrx#7.0/46.67124/8.29951"></iframe>
+<iframe width="1000" height="750" src="https://api.maptiler.com/maps/outdoor/?key=kVbYzZdvpCATj1RhoWrx#4.3/54.61178/-6.16035"></iframe>
+
+<html>
+  <head>
+    <script src="https://cdn.maptiler.com/maptiler-geocoder/v1.1.0/maptiler-geocoder.js"></script>
+    <link href="https://cdn.maptiler.com/maptiler-geocoder/v1.1.0/maptiler-geocoder.css" rel="stylesheet" />
+  </head>
+  <body>
+    <input autocomplete="off" id="search" type="text" placeholder="Search..."/>
+    <script>
+      var geocoder = new maptiler.Geocoder({
+        input: 'search',
+        key: 'kVbYzZdvpCATj1RhoWrx'
+      });
+      geocoder.on('select', function(item) {
+        console.log('Selected', item);
+        
+      });
+      
+      
+    </script>
+  </body>
+</html>
+
