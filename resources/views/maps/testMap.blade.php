d280d5318f4e03726e11fbdb7f03163d294519e9
SolitaireWolf <45793465+solitaryWolf987@users.noreply.github.com>
2022-03-03 13:48:02 +0000

    placemarkers

--- resources/views/maps/testMap.blade.php
+++ resources/views/maps/testMap.blade.php
@@ -1,26 +1,36 @@
 
 
-<iframe width="1000" height="750" src="https://api.maptiler.com/maps/outdoor/?key=kVbYzZdvpCATj1RhoWrx#4.3/54.61178/-6.16035"></iframe>
-
+<!DOCTYPE html>
 <html>
-  <head>
-    <script src="https://cdn.maptiler.com/maptiler-geocoder/v1.1.0/maptiler-geocoder.js"></script>
-    <link href="https://cdn.maptiler.com/maptiler-geocoder/v1.1.0/maptiler-geocoder.css" rel="stylesheet" />
-  </head>
-  <body>
-    <input autocomplete="off" id="search" type="text" placeholder="Search..."/>
-    <script>
-      var geocoder = new maptiler.Geocoder({
-        input: 'search',
-        key: 'kVbYzZdvpCATj1RhoWrx'
-      });
-      geocoder.on('select', function(item) {
-        console.log('Selected', item);
-        
-      });
-      
-      
-    </script>
-  </body>
-</html>
+<head>
+	<title>my map - london</title>
+	<meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
+    <script src="https://cdn.maptiler.com/maplibre-gl-js/v1.14.0/maplibre-gl.js"></script>
+    <link href="https://cdn.maptiler.com/maplibre-gl-js/v1.14.0/maplibre-gl.css" rel="stylesheet" />
+    <style>
+    	#map {
+        border: 1px solid #ddd;
+      border-radius: 4px;
+      padding: 5px;
+      width: 1000px;
+      height: 750px;
+      }
+    </style>
+</head>
+<body>
+	<div id="map"></div>
+	<script type="text/javascript">
+		var map = new maplibregl.Map({
+		container: 'map',
+		style: 'https://api.maptiler.com/maps/outdoor/style.json?key=kVbYzZdvpCATj1RhoWrx',
+		center: [0.1276, 51.5072],
+		zoom: 6,
+		});
 
+		//add marker
+		var london = new maplibregl.Marker()
+		.setLngLat([-0.1276, 51.5072])
+		.addTo(map);
+	</script>
+</body>
+</html>
